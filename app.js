297830b95e8bd9cc41c2df874fc44671a21e454b
Oh-Tae-Yang <tyo1012@naver.com>
2022-03-08 01:17:32 +0900

    Commit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 //모듈 추출
 
-var app = express();
-var server = http.createServer(app);
+const app = express();
+const port = 3000;
 //웹 서버 생성
 
-server.listen(52273, function() {
-    console.log('server running at http://127.0.0.1:52273');
+app.listen(port, function() {
+    console.log('server running at http://127.0.0.1:${port}');
 });//server 실행
 
-app.get('/main', function(request, response) {
+app.get('/', function(request, response) {
     fs.readFile("main.html", function(error, data) {
         response.writeHead(200, {"Content-Type" : "text/html"});
         response.end(data);
