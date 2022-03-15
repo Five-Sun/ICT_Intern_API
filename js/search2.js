e579fd7aee895424d2b8536344e9ca051a9c4e7c
Five-Sun <tyo1012@naver.com>
2022-03-15 17:53:29 +0900

    Commit

--- js/search2.js
+++ js/search2.js
@@ -1,27 +1,30 @@
-$(document).ready(function() {
-  var API_KEY="AIzaSyDQSVysWES54RDYnjZtfbmftS1K6WlBpVQ";
-  var video = ""
+function youtube() {
+  $(document).ready(function () {
+    var API_KEY = "AIzaSyDQSVysWES54RDYnjZtfbmftS1K6WlBpVQ";
+    var video = ""
 
-  $("form").submit(function(event) {
-    event.preventDefault()
+    $("title").click(function (event) {
+      event.preventDefault()
 
-    var search = $("#search").val()
+      var search = $("#title").val()
 
-    videoSearch(API_KEY,search,1)
+      videoSearch(API_KEY, search, 1)
+    })
+
+    function videoSearch(key, search, maxResults) {
+
+      $.get("https://www.googleapis.com/youtube/v3/search?key=" + key
+        + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function (data) {
+          console.log(data)
+
+          data.items.forEach(item => {
+            video = `
+          <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoID}" frameborder="0" allowfullscreen></iframe>
+          `
+            $("videos").append(video)
+          });
+        })
+    }
   })
 
-  function videoSearch(key, search, maxResults) {
-
-    $.get("https://www.googleapis.com/youtube/v3/search?key=" + key
-    + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data) {
-      console.log(data)
-
-      data.items.forEach(item => {
-        video = `
-        <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoID}" frameborder="0" allowfullscreen></iframe>
-        `
-        $("videos").append(video)
-      });
-    })
-  }
-})
+}
