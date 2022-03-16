88cae986926a49c8cf18b4a00f539bdcbd249bc0
Five-Sun <tyo1012@naver.com>
2022-03-16 16:20:04 +0900

    Commit

--- public/share.js
+++ public/share.js
@@ -21,9 +21,9 @@ function kakaoShare(data) {
                 title: '책 정보 보기',
                 link: {
                     mobileWebUrl: shareUrl,
-                    webUrl: shareUrl
+                   webUrl: shareUrl
                 },
-            }
+            },
         ]
     });
-} //카카오톡 공유하기 API 내용에 맞게 매개변수로 해당 정보의 객체를 받아 내용물로 지정
+}; //카카오톡 공유하기 API 내용에 맞게 매개변수로 해당 정보의 객체를 받아 내용물로 지정
